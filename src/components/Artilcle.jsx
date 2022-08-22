23c187df08b8495d887788b4facfbff2ede428eb
Mansur Mabo <mansurmabo@gmail.com>
2022-08-22 11:01:19 +0600

    add article

---
+++ src/components/Artilcle.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+export default class Article extends React.Component {
+  constructor(props) {
+    console.log('contstructor')
+    super(props);
+    this.handleShow = this.handleShow.bind(this);
+
+    this.state = {
+      show: false,
+    };
+  }
+
+  handleShow() {
+    this.setState({ 
+      show: !this.state.show 
+    });
+  }
+
+  componentDidMount() {
+    console.log('mount');
+  }
+
+  componentDidUpdate() {
+    console.log('reRender');
+  }
+
+  componentWillUnmount() {
+    console.log('unmount');
+  }
+
+  render() {
+    const renderArticleText = () => {
+      if (this.state.show) {
+        return <div>
+          Для этого добавим так называемое «состояние» (state) в компонент
+          Clock. «Состояние» очень похоже на уже знакомые нам пропсы, отличие
+          в том, что состояние контролируется и доступно только конкретному
+          компоненту.
+        </div>
+      } else {
+        return 'Loading....'
+      }
+    }
+
+    console.log('render');
+    return (
+      <div>
+        <button onClick={this.handleShow}>
+        {this.state.show ? 'Hide' : 'Show'}
+        </button>
+        {renderArticleText()}
+      </div>
+    );
+  }
+}
